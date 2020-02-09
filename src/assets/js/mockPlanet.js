113f3c844f3f0898c34c143beacfd26aaebf195f
Gabriel Gomes <gabriel_gom@hotmail.com>
2020-02-09 13:38:27 -0300

    Add: mockPlanet.js

---
+++ src/assets/js/mockPlanet.js
@@ -0,0 +1,15 @@
+const mockPlanet = {
+  name: 'Mock Planet',
+  population: '99999999',
+  climate: 'temperate, tropical',
+  terrain: 'grassy hills, swamps, forests, mountains',
+  films: [
+    'https://swapi.co/api/films/5/',
+    'https://swapi.co/api/films/4/',
+    'https://swapi.co/api/films/6/',
+    'https://swapi.co/api/films/3/',
+    'https://swapi.co/api/films/1/',
+  ],
+};
+
+export default mockPlanet;
