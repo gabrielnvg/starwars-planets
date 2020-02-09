136a03a752615b90f4f4610d59fdfc65d34beaa4
Gabriel Gomes <gabriel_gom@hotmail.com>
2020-02-08 22:35:00 -0300

    Add: Header component concluded

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Header.module.scss';
 
-import logo from '../../assets/images/svg/star-wars-logo.svg';
+import imgLogo from '../../assets/images/svg/star-wars-logo.svg';
+import imgSeparator from '../../assets/images/svg/separator.svg';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <img className={styles.logo} src={logo} alt="logo" />
-      <h1>Star Wars Planets</h1>
+      <div className="main-container">
+        <img className={styles.logo} src={imgLogo} alt="logo" />
+        <div className={styles['text-container']}>
+          <h1 className={styles.text}>Random Planets</h1>
+          <img className={styles.separator} src={imgSeparator} alt="separator" />
+        </div>
+      </div>
     </header>
   );
 }
