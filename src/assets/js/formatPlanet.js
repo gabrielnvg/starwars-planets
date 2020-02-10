ed0471f19a851aaec052326f7bbd6403c437d062
Gabriel Gomes <gabriel_gom@hotmail.com>
2020-02-10 02:43:34 -0300

    Add: Failsafes on formatPlanet.js

--- src/assets/js/formatPlanet.js
+++ src/assets/js/formatPlanet.js
@@ -1,14 +1,32 @@
 const addDotsOnNumber = number => new Intl.NumberFormat().format(number);
 
-const commaSeparatedStringToArray = string => string.split(", ");
+const commaSeparatedStringToArray = string => string.split(', ');
 
 const formatPlanet = fetchedPlanet => {
+  let name = '???';
+  if (fetchedPlanet.name && fetchedPlanet.name !== 'unknown') {
+    name = fetchedPlanet.name;
+  }
+  let population = '???';
+  if (fetchedPlanet.population && fetchedPlanet.population !== 'unknown') {
+    population = addDotsOnNumber(fetchedPlanet.population);
+  }
+  let climate = ['???'];
+  if (fetchedPlanet.climate && fetchedPlanet.climate !== 'unknown') {
+    climate = commaSeparatedStringToArray(fetchedPlanet.climate);
+  }
+  let terrain = ['???'];
+  if (fetchedPlanet.terrain && fetchedPlanet.terrain !== 'unknown') {
+    terrain = commaSeparatedStringToArray(fetchedPlanet.terrain);
+  }
+  const films = fetchedPlanet.films ? fetchedPlanet.films.length : 0;
+
   return {
-    name: fetchedPlanet.name,
-    population: addDotsOnNumber(fetchedPlanet.population),
-    climate: commaSeparatedStringToArray(fetchedPlanet.climate),
-    terrain: commaSeparatedStringToArray(fetchedPlanet.terrain),
-    films: fetchedPlanet.films.length,
+    name,
+    population,
+    climate,
+    terrain,
+    films,
   };
 };
 
