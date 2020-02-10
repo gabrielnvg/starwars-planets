4af3aba1fa0d4e3c1ea0f3f1e4a53f530c766b8d
Gabriel Gomes <gabriel_gom@hotmail.com>
2020-02-10 00:28:15 -0300

    Add: FetchLoading component created

---
+++ src/components/FetchLoading/FetchLoading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './FetchLoading.module.scss';
+
+function FetchLoading() {
+  return (
+    <div className={styles['fetch-loading']}>
+      Loading...
+    </div>
+  );
+}
+
+export default FetchLoading;
