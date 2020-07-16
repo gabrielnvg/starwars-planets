f8e67af801880a0ada7bf7cee7e6265b24e86734
Gabriel Valverde <gabriel_gom@hotmail.com>
2020-07-16 00:53:30 -0300

    Change: FetchError is now a pure component

--- src/components/FetchError/FetchError.js
+++ src/components/FetchError/FetchError.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './FetchError.module.scss';
 
 function FetchError() {
   return (
     <div className={styles['fetch-error']}>
       <div className={styles['text-row']}>Could not get a planet.</div>
-      <div className={styles['text-row']}>Please check your connection and try again later.</div>
+      <div className={styles['text-row']}>
+        Please check your connection and try again later.
+      </div>
     </div>
   );
 }
 
-export default FetchError;
+export default memo(FetchError);
