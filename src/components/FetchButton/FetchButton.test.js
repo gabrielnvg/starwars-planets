f61517bb14567a1165e8de0e8d4f19f6c5e27a72
Gabriel Valverde <gabriel_gom@hotmail.com>
2020-07-16 02:38:18 -0300

    Add: Test for FetchButton click event

--- src/components/FetchButton/FetchButton.test.js
+++ src/components/FetchButton/FetchButton.test.js
@@ -3,15 +3,20 @@ import { shallow } from '../../setupTests';
 
 import FetchButton from './FetchButton';
 
+const clickResult = 'The button was clicked!';
+const mockCallback = jest.fn(() => clickResult);
+
 describe('FetchButton component', () => {
+  const wrapper = shallow(
+    <FetchButton text="Text" onClick={mockCallback} isDisabled={false} />,
+  );
+
   it('renders without crashing', () => {
-    const wrapper = shallow(
-      <FetchButton
-        text="Text"
-        onClick={() => null}
-        isDisabled={false}
-      />,
-    );
     expect(wrapper).toHaveLength(1);
+  });
+
+  it('simulates click events', () => {
+    wrapper.find('button').simulate('click');
+    expect(mockCallback.mock.results[0].value).toBe(clickResult);
   });
 });
