0d627c998e07bb46c9ca946c288c140925402626
Gabriel Valverde <gabriel_gom@hotmail.com>
2020-07-19 18:43:49 -0300

    Change: Using custom fetches instead of use-http

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import useFetch from 'use-http';
 import randomizeIntWithinRange from './assets/js/utils/randomizeIntWithinRange';
 import formatPlanet from './assets/js/utils/formatPlanet';
 
@@ -10,48 +9,97 @@ import PlanetInfo from './components/PlanetInfo/PlanetInfo';
 import FetchButton from './components/FetchButton/FetchButton';
 
 function App() {
+  const [state, setState] = useState({
+    fetchStatus: {
+      isLoading: true,
+      hasError: false,
+    },
+    fetchedPlanets: {},
+    currentFetchedPlanet: {},
+  });
   const [totalPlanets, setTotalPlanets] = useState(0);
-  const [currentFetchedPlanet, setCurrentFetchedPlanet] = useState({});
-  const { get, response, loading, error } = useFetch('https://swapi.dev/api');
-  const useMountEffect = func => useEffect(func, []);
+  const useMountEffect = callbackFunction => useEffect(callbackFunction, []);
+
+  const apiPrefix = 'https://swapi.dev/api';
 
   async function fetchRandomPlanet(planetId) {
-    const planet = await get(`/planets/${planetId}/`);
-    if (response.ok) {
-      setCurrentFetchedPlanet(planet);
+    if (planetId in state.fetchedPlanets) {
+      setTimeout(
+        () =>
+          setState({
+            ...state,
+            fetchStatus: { isLoading: false, hasError: false },
+            currentFetchedPlanet: state.fetchedPlanets[planetId],
+          }),
+        300,
+      );
+    } else {
+      await fetch(`${apiPrefix}/planets/${planetId}/`)
+        .then(response => response.json())
+        .then(planet => {
+          setState({
+            ...state,
+            fetchStatus: { isLoading: false, hasError: false },
+            fetchedPlanets: {
+              ...state.fetchedPlanets,
+              [planetId]: planet,
+            },
+            currentFetchedPlanet: planet,
+          });
+        })
+        .catch(() => {
+          setState({
+            ...state,
+            fetchStatus: { isLoading: false, hasError: true },
+          });
+        });
     }
   }
 
-  const handleButtonClick = () => {
-    fetchRandomPlanet(randomizeIntWithinRange(1, totalPlanets));
-  };
-
   useMountEffect(() => {
     async function initialFetches() {
-      const planets = await get('/planets/');
-      if (response.ok) {
-        setTotalPlanets(planets.count);
-        fetchRandomPlanet(randomizeIntWithinRange(1, planets.count));
-      }
+      await fetch(`${apiPrefix}/planets/`)
+        .then(response => response.json())
+        .then(planets => {
+          setTotalPlanets(planets.count);
+          fetchRandomPlanet(randomizeIntWithinRange(1, planets.count));
+        })
+        .catch(() => {
+          setState({
+            ...state,
+            fetchStatus: { isLoading: false, hasError: true },
+          });
+        });
     }
     initialFetches();
   });
+
+  const handleButtonClick = () => {
+    setState({
+      ...state,
+      fetchStatus: { isLoading: true, hasError: false },
+    });
+
+    fetchRandomPlanet(randomizeIntWithinRange(1, totalPlanets));
+  };
 
   return (
     <div>
       <Header />
       <div className="main-container">
         <div className="central-container">
-          {loading && <FetchLoading />}
-          {!loading && error && <FetchError />}
-          {!loading && !error && (
-            <PlanetInfo planet={formatPlanet(currentFetchedPlanet)} />
+          {state.fetchStatus.isLoading && <FetchLoading />}
+          {!state.fetchStatus.isLoading && state.fetchStatus.hasError && (
+            <FetchError />
+          )}
+          {!state.fetchStatus.isLoading && !state.fetchStatus.hasError && (
+            <PlanetInfo planet={formatPlanet(state.currentFetchedPlanet)} />
           )}
         </div>
         <FetchButton
           text="Next"
           onClick={handleButtonClick}
-          isDisabled={loading}
+          isDisabled={state.fetchStatus.isLoading}
         />
       </div>
     </div>
